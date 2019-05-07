0d951d0fa76ac2953fd3cc636c572b42770eb359
diwal123 <diwalpyakurel@gmail.com>
2019-05-07 18:15:59 -0500

    Change

---
+++ build/precache-manifest.1dadfb5cf720cb5cb68e63929237a499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45c9212717b449b3bdbee60147c99bf",
+    "url": "/Clicky-Avengers/index.html"
+  },
+  {
+    "revision": "32a625a11f45749b1df9",
+    "url": "/Clicky-Avengers/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "3ac859821251d9dbf684",
+    "url": "/Clicky-Avengers/static/js/2.69fc8f4e.chunk.js"
+  },
+  {
+    "revision": "32a625a11f45749b1df9",
+    "url": "/Clicky-Avengers/static/js/main.1bf32ad6.chunk.js"
+  },
+  {
+    "revision": "3726493f3cc585becbc3",
+    "url": "/Clicky-Avengers/static/js/runtime~main.36d166b3.js"
+  }
+]);
